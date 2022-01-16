e7eb5cc4083783285ed488aa5875a83b26c1ee46
boguszpawlowski <bogusz.pawlowski@gmail.com>
2022-01-16 18:00:12 +0100

    Add basic KDocs for the API

--- library/src/commonMain/kotlin/io/github/boguszpawlowski/chassis/Field.kt
+++ library/src/commonMain/kotlin/io/github/boguszpawlowski/chassis/Field.kt
@@ -2,13 +2,50 @@ package io.github.boguszpawlowski.chassis
 
 import io.github.boguszpawlowski.chassis.ValidationStrategy.AsOptional
 
+/**
+ * A core component of a library. It represents a single field in your validation, it's value and validation state. It is also handling any updates to the
+ * field itself. Note that you shouldn't use `update`, `forceValidation` functions on your own, as this are just a pure functions returning a new instance of the interface, and thus not updating the state.
+ * Instead, you should use it's counterparts on a `Chassis` interface.
+ */
 public interface Field<T : Any, V : Any?> {
+  /**
+   * Current value of the field.
+   */
   public val value: V?
+
+  /**
+   * Whenever field is valid or not.
+   */
   public val isValid: Boolean
+
+  /**
+   * Whenever field is invalid or not.
+   * Syntactic sugar over `invalidReasons.isNotEmpty()`
+   */
   public val isInvalid: Boolean
+
+  /**
+   * List of reasons for failed validation. If you force the validation, the invalid cause of it will be appended to this list.
+   */
   public val invalidReasons: List<Invalid>
+
+  /**
+   * An effectively internal function for forcing the validation on the field. Use `forceValidation` of the chassis interface instead of it.
+   * @return a new instance of the field
+   * @param state current state of your model
+   * @param validationResult validation result which you want to force
+   */
   public fun forceValidation(state: T, validationResult: ValidationResult): T
+
+  /**
+   * An effectively internal function for updating the fields value. Use `update` of the chassis interface instead of it.
+   * @return new instance of the field
+   */
   public fun reduce(state: T, newValue: V?): T
+
+  /**
+   * A function that will return current value casted to fields type. Use it only after the validation is completed.
+   */
   public operator fun invoke(): V
 }
 
--- library/src/commonMain/kotlin/io/github/boguszpawlowski/chassis/Validator.kt
+++ library/src/commonMain/kotlin/io/github/boguszpawlowski/chassis/Validator.kt
@@ -1,13 +1,44 @@
 package io.github.boguszpawlowski.chassis
 
+/**
+ * A functional interface of a smallest component.
+ */
 public fun interface Validator<T> : (T) -> ValidationResult
 
+/**
+ * A root interface for the validation results class hierarchy. You should never directly implement it. Use `Invalid` for defining your own validation failure domain.
+ */
 public sealed interface ValidationResult
-public object Unspecified : ValidationResult
+
+/**
+ * A default validation state of the required fields, before any user input.
+ */
+internal object Unspecified : ValidationResult
+
+/**
+ * A class representing valid state of the field.
+ */
 public object Valid : ValidationResult
+
+/**
+ * A root interface for the validation failure class hierarchy. Implement it in order to return you custom failures from the validators.
+ */
 public interface Invalid : ValidationResult
+
+/**
+ * Default invalid cause, will be returned by a built-in library validators. It doesn't provide any context or data about the failure.
+ */
 public object DefaultInvalid : Invalid
 
+/**
+ * A function that allows to compose independent validators.
+ *
+ * login = field {
+ *   validators(shorterThan(2) or longerThan(10))
+ *   ...
+ * }
+ * Above field will be valid with input shorter than 2 or longer than 10 chars.
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline infix fun <T> Validator<T>.or(other: Validator<T>): Validator<T> = Validator {
   if (this(it) == Valid) {
@@ -17,16 +48,10 @@ public inline infix fun <T> Validator<T>.or(other: Validator<T>): Validator<T> =
   }
 }
 
-@Suppress("NOTHING_TO_INLINE")
-public inline infix fun <T> Validator<T>.and(other: Validator<T>): Validator<T> = Validator {
-  val result = this(it)
-  if (result == Valid) {
-    other(it)
-  } else {
-    result
-  }
-}
-
+/**
+ * A validator, which will return Valid for every non-empty input
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun notEmpty(invalid: Invalid = DefaultInvalid): Validator<String?> = Validator {
   if (it.isNullOrEmpty().not()) {
     Valid
@@ -35,6 +60,11 @@ public fun notEmpty(invalid: Invalid = DefaultInvalid): Validator<String?> = Val
   }
 }
 
+/**
+ * A validator, which will return Valid for every input shorter than specified number of chars.
+ * @param max maximum allowed number of chars
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun shorterThan(max: Int, invalid: Invalid = DefaultInvalid): Validator<String?> =
   Validator {
     if (it.orEmpty().length < max) {
@@ -44,6 +74,11 @@ public fun shorterThan(max: Int, invalid: Invalid = DefaultInvalid): Validator<S
     }
   }
 
+/**
+ * A validator, which will return Valid for every input longer than specified number of chars.
+ * @param min minimum allowed number of chars
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun longerThan(min: Int, invalid: Invalid = DefaultInvalid): Validator<String?> = Validator {
   if (it.orEmpty().length > min) {
     Valid
@@ -52,6 +87,11 @@ public fun longerThan(min: Int, invalid: Invalid = DefaultInvalid): Validator<St
   }
 }
 
+/**
+ * A validator, which will return Valid only if the size of the input will be exactly the provided size
+ * @param length required length of the input
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun exactly(length: Int, invalid: Invalid = DefaultInvalid): Validator<String?> = Validator {
   if (it.orEmpty().length == length) {
     Valid
@@ -60,6 +100,11 @@ public fun exactly(length: Int, invalid: Invalid = DefaultInvalid): Validator<St
   }
 }
 
+/**
+ * A validator, which will return Valid for every input matching the provided regex
+ * @param regex a regex for matching the input
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun matches(regex: Regex, invalid: Invalid = DefaultInvalid): Validator<String?> =
   Validator {
     if (regex.matches(it.orEmpty())) {
@@ -69,7 +114,11 @@ public fun matches(regex: Regex, invalid: Invalid = DefaultInvalid): Validator<S
     }
   }
 
-public fun notNull(invalid: Invalid = DefaultInvalid): Validator<Any?> = Validator {
+/**
+ * A validator, which will return Valid for every non-null input
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
+public fun <T : Any?> notNull(invalid: Invalid = DefaultInvalid): Validator<T> = Validator {
   if (it != null) {
     Valid
   } else {
@@ -77,6 +126,10 @@ public fun notNull(invalid: Invalid = DefaultInvalid): Validator<Any?> = Validat
   }
 }
 
+/**
+ * A validator, which will return Valid for a `true` as the input
+ * @param invalid a `Invalid` interface instance which will be returned on failure.
+ */
 public fun required(invalid: Invalid = DefaultInvalid): Validator<Boolean?> = Validator {
   if (it == true) {
     Valid
